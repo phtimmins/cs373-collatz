6946f9b0bea87efc40944bf6e1daa5d1248674ef
Parker Timmins <parkert@cs.utexas.edu>
2015-02-05 10:06:55 -0600

    Combined Collatz.py and RunCollatz.py into SphereCollatz.py, ran on Sphere, closes #14

---
+++ SphereCollatz.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+
+# ---------------------------
+# projects/collatz/SphereCollatz.py
+# Copyright (C) 2015
+# Glenn P. Downing
+# ---------------------------
+
+# -------
+# imports
+# -------
+
+import sys
+
+# ------------
+# collatz_read
+# ------------
+
+def collatz_read (s) :
+    """
+    read two ints
+    s a string
+    return a list of two ints, representing the beginning and end of a range, [i, j]
+    """
+    a = s.split()
+    return [int(a[0]), int(a[1])]
+
+# ------------
+# collatz_eval
+# ------------
+
+def collatz_eval (i, j) :
+    """
+    i the beginning of the range, inclusive
+    j the end       of the range, inclusive
+    return the max cycle length of the range [i, j]
+    """
+    assert i >= 1
+    assert j >= 1
+
+    if i > j:
+        i, j = j, i
+
+    assert i <= j
+
+    result = max(collatz_len(x) for x in range(i, j + 1))
+    assert result >= 1
+    return result
+    
+
+def collatz_len (n) :
+    assert n >= 1
+    if n == 1:
+        length = 1
+    elif n % 2 == 0:
+        length = 1 + collatz_len(n // 2)
+    else:
+        length = 1 + collatz_len(3 * n + 1)
+    assert length >= 1
+    return length
+
+
+# -------------
+# collatz_print
+# -------------
+
+def collatz_print (w, i, j, v) :
+    """
+    print three ints
+    w a writer
+    i the beginning of the range, inclusive
+    j the end       of the range, inclusive
+    v the max cycle length
+    """
+    w.write(str(i) + " " + str(j) + " " + str(v) + "\n")
+
+# -------------
+# collatz_solve
+# -------------
+
+def collatz_solve (r, w) :
+    """
+    r a reader
+    w a writer
+    """
+    for s in r :
+        i, j = collatz_read(s)
+        v    = collatz_eval(i, j)
+        collatz_print(w, i, j, v)
+
+# ----
+# main
+# ----
+
+if __name__ == "__main__" :
+    collatz_solve(sys.stdin, sys.stdout)
+
